8a9a2fff084c319a41242cb064386b8d1a776e94
rodgers-munene <munenerodgers72@gmail.com>
2025-07-25 16:16:57 +0300

    hotel data loaded dynamically

--- lib/base/res/media.dart
+++ lib/base/res/media.dart
@@ -2,4 +2,5 @@ class AppMedia {
   static const _baseImage = "assets/images";
 
   static const logo = "$_baseImage/logo.png";
+  static const hotelImages = "$_baseImage/hotel_room.png";
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/utils/app_json.dart';
 import 'package:ticket_app/base/widgets/app_double_text.dart';
 import 'package:ticket_app/base/widgets/ticket_view.dart';
+import 'package:ticket_app/screens/widgets/hotel.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -16,10 +17,11 @@ class HomeScreen extends StatelessWidget {
       // scrollable screen
       body: ListView(
         children: [
-          SizedBox(height: 40,),
+          SizedBox(height: 40),
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -27,71 +29,79 @@ class HomeScreen extends StatelessWidget {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          "Good Morning",
-                          style: AppStyles.headLineStyle3
-                        ),
+                        Text("Good Morning", style: AppStyles.headLineStyle3),
                         SizedBox(height: 5),
-                        Text(
-                          "Book Tickets",
-                          style: AppStyles.headLineStyle1,
-                        ),
+                        Text("Book Tickets", style: AppStyles.headLineStyle1),
                       ],
                     ),
                     Container(
-                      width: 50, 
-                      height: 50, 
+                      width: 50,
+                      height: 50,
                       decoration: BoxDecoration(
-                        image: DecorationImage(image: AssetImage(AppMedia.logo)),
-                        borderRadius: BorderRadius.circular(10)
+                        image: DecorationImage(
+                          image: AssetImage(AppMedia.logo),
+                        ),
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      
                     ),
                   ],
                 ),
-                SizedBox(height: 25,),
+                SizedBox(height: 25),
                 Container(
                   padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
                   decoration: BoxDecoration(
-                        color: const Color(0xFFF4F6FD),
-                        borderRadius: BorderRadius.circular(10)
-                    ),
+                    color: const Color(0xFFF4F6FD),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                   child: Row(
-                   
                     children: [
-                      Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205),),
-                  
-                      Text("Search")],
+                      Icon(
+                        FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color(0xFFBFC205),
+                      ),
+
+                      Text("Search"),
+                    ],
                   ),
                 ),
-                SizedBox(height: 40,),
+                SizedBox(height: 40),
                 AppDoubleText(
-                  bigText: "Upcoming Flights", 
+                  bigText: "Upcoming Flights",
                   smallText: "View all",
                   func: () {
                     Navigator.pushNamed(context, "/all_tickets");
-                  }
+                  },
                 ),
-                SizedBox(height: 20,),
+                SizedBox(height: 20),
                 SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
                   child: Row(
-                    children: ticketList.take(2).map((item) => TicketView(ticket: item)).toList()
-                    ,
+                    children: ticketList
+                        .take(2)
+                        .map((item) => TicketView(ticket: item))
+                        .toList(),
                   ),
                 ),
-                SizedBox(height: 40,),
+                SizedBox(height: 40),
                 AppDoubleText(
-                  bigText: "Hotels", 
+                  bigText: "Hotels",
                   smallText: "View all",
                   func: () {
                     Navigator.pushNamed(context, "/all_hotels");
                   },
-                  )
+                ),
+                SizedBox(height: 20),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(children: hotelList
+                        .take(2)
+                        .map((item) => Hotel(hotel: item))
+                        .toList(),
+                  ),
+                ),
               ],
             ),
           ),
-
         ],
       ),
     );
